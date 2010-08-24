7c08574adb4af452bdd2cdb79793f2aa52879f8c
Robert Lehmann <me@robertlehmann.de>
2010-08-24 12:47:54 +0200

    Documented Card class.

--- cardshuffle/cards/__init__.py
+++ cardshuffle/cards/__init__.py
@@ -10,17 +10,24 @@ class Card:
     by_tag = defaultdict(list)
     class __metaclass__(type):
         def __new__(meta, name, bases, clsdict):
+            # extract the card name from its class name if it was not
+            # explicitly set
             clsdict.setdefault('name', TITLECASE.sub(r' \1', name))
+            # transform its tags string into a list of tags
             clsdict['tags'] = tags = clsdict.get('tags', "").split()
+            # ..and add all its superclass tags, too
             for base in bases:
                 tags.extend(base.tags)
+            # create the Card type
             new = type.__new__(meta, name, bases, clsdict)
+            # add the card to all of its tags in the `by_tag` mapping
             for tag in tags:
                 new.by_tag[tag].append(new)
             return new
 
     @classmethod
     def all(cls):
+        """Return all cards known to the game engine."""
         #XXX caching
         children = cls.__subclasses__()
         for child in children:
