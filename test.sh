291fed8cfc4a0818c682303f97239ce019a1a750
Robert Lehmann <me@robertlehmann.de>
2010-08-24 13:08:57 +0200

    Updated test script to new ruleset configuration API.

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ VIRTUALENV=var
 test -d "$VIRTUALENV" || virtualenv --distribute --no-site-packages "$VIRTUALENV"
 . "$VIRTUALENV/bin/activate"
 python setup.py install
-twistd -n cardshuffle -n 3 &
-sleep 1.5
+twistd -n cardshuffle --players=3 &
+sleep 1.5 # wait for the server to start up
 echo -e "random\nready" | nc localhost 1030 >/dev/null &
 echo -e "random\nready" | nc localhost 1030 >/dev/null &
-nc localhost 1030
-sleep 0.5
+nc localhost 1030 # run until human player disconnects
+sleep 0.5 # wait for the server to tear down
 kill %1 %2 %3
