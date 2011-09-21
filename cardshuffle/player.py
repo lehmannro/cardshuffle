cae2a9e5a4ecc60f42465de33b5ce5190dc99480
Robert Lehmann <mail@robertlehmann.de>
2011-09-21 08:01:12 +0200

    Wrapped HP/MP/DP in managed attributes.

--- cardshuffle/player.py
+++ cardshuffle/player.py
@@ -1,35 +1,51 @@
 # Copyright (C) 2010 Robert Lehmann
 
 import random
+import operator
 
 class Player(object):
     def __init__(self, name, deck, handsize, mana, health, draws):
         self.name = name
         self.deck = deck[:]
         self.max_mana = mana
+        self.max_health = health
         # managed properties
-        self.mana = 0
-        self.health = health
-        self.draws = draws
+        self._mana = 0
+        self._health = health
+        self._draws = draws
 
         # We could possibly deal all cards but players might wish to draw cards
         # at their leisure, eg. to avoid spells on their hand.
         ## self.hand = random.sample(self.deck, handsize)
         self.hand = [None] * handsize
 
+    @apply
+    def mana():
+        fget = operator.attrgetter('_mana')
+        def fset(self, value):
+            self._mana = min(max(self._mana, 0), self.max_mana)
+        return property(fget, fset)
+
+    @apply
+    def health():
+        fget = operator.attrgetter('_health')
+        def fset(self, value):
+            alive = self.alive
+            self._health = min(max(self._health, 0), self.max_health)
+            if alive != self.alive:
+                pass #XXX player died (or was resurrected), notify someone?
+        return property(fget, fset)
+
+    @apply
+    def draws():
+        fget = operator.attrgetter('_draws')
+        def fset(self, value):
+            self._draws = min(max(self._draws, 0), self.max_health)
+        return property(fget, fset)
+
     @property
     def alive(self):
         return self.health != 0
-
-    #XXX this should be wrapped in managed attributes
-    def award_mana(self):
-        self.mana = min(self.mana + 1, self.max_mana)
-    def award_drawpoint(self):
-        #XXX rules for draw points
-        self.draws += 1
-
-    def inflict(self, damage):
-        self.health = max(self.health - damage, 0)
 
     def draw(self):
         if None not in self.hand or not self.draws:
